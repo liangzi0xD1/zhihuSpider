3c1717a7909acd3ea24e3086819c75e9d4d58140
Sophos <hnlq.sysu@gmail.com>
2015-11-23 18:11:48 +0800

    improve savePost

--- savePost.go
+++ savePost.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"html/template"
+	"os/exec"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
@@ -9,14 +10,19 @@ import (
 	"time"
 )
 
-type PageContent struct {
-	Id       string
-	Name     string
-	Sid      int
+type QuestionContent struct {
 	Title    string
-	Agree    int
+	QuestionId string
+	QuestionLink string
+	Answers []AnswerContent
+}
+
+type AnswerContent struct {
 	Answerid string
 	Link     string
+	Id       string
+	Name     string
+	Agree    int
 	Ispost   int
 	Noshare  int
 	Length   int
@@ -48,7 +54,7 @@ func doSavePage() error {
 		yesterdayPage = true
 	}
 
-	rows, err := db.Query(`select b.id, u.name, b.sid, b.title, b.agree-a.agree agree, b.answerid, b.link, b.ispost, b.noshare, b.len, b.summary, u.avatar
+	rows, err := db.Query(`select b.id, u.name, b.title, b.agree-a.agree agree, b.answerid, b.link, b.ispost, b.noshare, b.len, b.summary, u.avatar
 									from zhihu.usertopanswers a
 									INNER JOIN zhihu.usertopanswers b on b.id=a.id and b.answerid = a.answerid and b.sid=a.sid+?
 									INNER JOIN zhihu.users u on u.id = a.id
@@ -60,36 +66,62 @@ func doSavePage() error {
 
 	day := strings.Split(startAtTime.Format(time.RFC3339), "T")[0]
 
+	path := "source/_posts/"
 	postTitle := "近日精选 "
-	fileName := day + "-today" + ".md"
+	fileName := path+day + "-today" + ".md"
 	if yesterdayPage {
 		postTitle = "昨日精选 "
-		fileName = day + "-yesterday" + ".md"
+		fileName = path+day + "-yesterday" + ".md"
 	}
 
-	const tpl = `title: {{.PTitle}}
-date: {{.Date}}
-tags:
----
-<ul>{{range .Contents}}<li><h3><a href="{{.Link}}">{{.Title}}</a></h3><a href="http://zhihu.com/people/{{.Id}}"><img src="{{.Avatar}}" align="left"></a><span>**[{{.Name}}](http://zhihu.com/people/{{.Id}})**: (*{{.Agree}}* 新增赞同){{.Summary}}</span></li><div style="clear: both; margin-bottom: 16px;"></div>{{end}}</ul>`
-
-	t, err := template.New("post").Parse(tpl)
-	if err != nil {
-		return err
-	}
-
-	contents := []PageContent{}
+	contents := []QuestionContent{}
 	for rows.Next() {
-		var c PageContent
+		var a AnswerContent
+		var q QuestionContent
 
-		rows.Scan(&c.Id, &c.Name, &c.Sid, &c.Title, &c.Agree, &c.Answerid, &c.Link, &c.Ispost, &c.Noshare, &c.Length, &c.Summary, &c.Avatar)
+		rows.Scan(&a.Id, &a.Name, &q.Title, &a.Agree, &a.Answerid, &a.Link, &a.Ispost, &a.Noshare, &a.Length, &a.Summary, &a.Avatar)
 
-		log.Printf("id:%s, name:%s, sid:%d, title:%s, agree:%d, answerid:%s, link:%s, ispost:%d, noshare:%d, length:%d, summary:%s, avatar:%s",
-			c.Id, c.Name, c.Sid, c.Title, c.Agree, c.Answerid, c.Link, c.Ispost, c.Noshare, c.Length, c.Summary, c.Avatar)
-		c.Summary = strings.TrimRight(strings.TrimLeft(c.Summary, "\n"), "\n")
+		a.Summary = strings.TrimRight(strings.TrimLeft(a.Summary, "\n"), "\n")
 		re := regexp.MustCompile("https://.*.zhimg.com")
-		c.Avatar = re.ReplaceAllString(c.Avatar, "http://avatar.wanzhihu.com")
-		contents = append(contents, c)
+		a.Avatar = re.ReplaceAllString(a.Avatar, "http://7xojdu.com1.z0.glb.clouddn.com")
+		q.QuestionId = strings.Split(a.Link, "/")[len(strings.Split(a.Link, "/"))-3]
+		q.QuestionLink = "http://www.zhihu.com/question/"+q.QuestionId
+
+		//log.Printf("id:%s, name:%s, title:%s, agree:%d, answerid:%s, link:%s, ispost:%d, noshare:%d, length:%d, summary:%s, avatar:%s",
+		//	a.Id, a.Name, q.Title, a.Agree, a.Answerid, a.Link, a.Ispost, a.Noshare, a.Length, a.Summary, a.Avatar)
+		
+		isFind := false
+		for k, v := range contents {
+			if v.QuestionId == q.QuestionId {
+				log.Println("found ", v.QuestionId)
+				isFind = true
+				contents[k].Answers = append(v.Answers, a)
+			}
+		}
+
+		if !isFind {
+			log.Println("not found ", q.QuestionId)
+			q.Answers = append(q.Answers, a)
+			contents = append(contents, q)
+		}
+	}
+
+	log.Println(len(contents))
+	postPage := fmt.Sprintf("title: %s\n", postTitle + day)
+	postPage += fmt.Sprintf("date: %s\n", day)
+	postPage += fmt.Sprintf("---\n")
+	for k1, v := range contents {
+		postPage += fmt.Sprintf("<h3 id='%s'><a href='%s'>%s</a></h3>", v.Title, v.QuestionLink, v.Title)
+		postPage += fmt.Sprintf("<div>")
+		for k2, a := range v.Answers {
+			log.Println("answers:", k1, k2)
+			postPage += fmt.Sprintf("<a href='http://zhihu.com/people/%s'>", a.Id)
+			postPage += fmt.Sprintf("<img src='%s' align='left'></a>", a.Avatar)
+			postPage += fmt.Sprintf("<span>**[%s](http://zhihu.com/people/%s)**: (*%d* 新增赞同)%s [阅读全文](%s)</span>", a.Name, a.Id, a.Agree, a.Summary, a.Link)
+			postPage += fmt.Sprintf("<div style='clear: both; margin-bottom: 16px;'></div>")
+		}
+		postPage += fmt.Sprintf("</div>")
+		postPage += fmt.Sprintf("\n")
 	}
 
 	file, err := os.Create(fileName)
@@ -98,21 +130,49 @@ tags:
 	}
 	defer file.Close()
 
-	type Data struct {
-		PTitle   string
-		Date     string
-		Contents []PageContent
-	}
-	var data Data
-	data.PTitle = postTitle + day
-	data.Date = day
-	data.Contents = contents
+	file.WriteString(postPage)
 
-	err = t.Execute(file, data)
+	log.Println("savePage done")
+	
+	err = deploy()
+	log.Println(err)
+	return err
+}
+
+func deploy() error {
+	c := exec.Command("hexo", "g")
+	err := c.Start()
 	if err != nil {
 		return err
 	}
 
-	log.Println("savePage done")
+	log.Printf("Waiting for command to finish...")
+	err = c.Wait()
+	if err != nil {
+		return err
+	}
+	c = exec.Command("hexo", "g")
+	err = c.Start()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Waiting for command to finish...")
+	err = c.Wait()
+	if err != nil {
+		return err
+	}
+
+	c = exec.Command("hexo", "d")
+	err = c.Start()
+	if err != nil {
+		return err
+	}
+
+	err = c.Wait()
+	if err != nil {
+		return err
+	}
+	
 	return err
 }
